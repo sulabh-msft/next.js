f090a86225887019e105e8d9f2d797d3e9029a3b
Sulabh Upadhyay <suupadhy@microsoft.com>
2022-06-01 15:09:06 +0530

    Update layout.js

--- examples/layout-component/components/layout.js
+++ examples/layout-component/components/layout.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from './layout.module.css'
 
+/**
 export default function Layout({ children }) {
   return (
     <>
@@ -8,6 +9,26 @@ export default function Layout({ children }) {
         <title>Layouts Example</title>
       </Head>
       <main className={styles.main}>{children}</main>
+    </>
+  )
+}
+**/
+
+import useSWR from 'swr'
+import Navbar from './navbar'
+import Footer from './footer'
+
+export default function Layout({ children }) {
+  const { data, error } = useSWR('/api/navigation', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
+  return (
+    <>
+      <Navbar links={data.links} />
+      <main className={styles.main}>{children}</main>
+      <Footer />
     </>
   )
 }
