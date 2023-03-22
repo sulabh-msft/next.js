a17d9b50e27531e864e3b47fed4dcdb6029622e2
Sulabh Upadhyay <suupadhy@microsoft.com>
2023-03-22 15:25:41 +0530

    Update index.js

--- examples/ssr-caching/pages/index.js
+++ examples/ssr-caching/pages/index.js
@@ -1,7 +1,7 @@
 export default function Index({ time }) {
   return (
     <main>
-      <h1>SSR Caching with Next.js</h1>
+      <h1>SSR Caching with Next.js 12</h1>
       <time dateTime={time}>{time}</time>
     </main>
   )
